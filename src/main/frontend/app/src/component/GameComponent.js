5ef83639c8a981c25a4b8e11512a29f71f999039
Nika Hoić <nh52536@fer.hr>
2022-01-16 20:45:55 +0100

    UPDATE

--- src/main/frontend/app/src/component/GameComponent.js
+++ src/main/frontend/app/src/component/GameComponent.js
@@ -46,6 +46,10 @@ class GameComponent extends Component {
             odgovor2: '',
             odgovor3: '',
             odgovor4: '',
+            tocan1:'',
+            tocan2:'',
+            tocan3:'',
+            tocan4:'',
             chosenOne: undefined,
             kraj: false
         }
@@ -76,11 +80,18 @@ class GameComponent extends Component {
                     odgovor2: allAnswers.data[1].text,
                     odgovor3: allAnswers.data[2].text,
                     odgovor4: allAnswers.data[3].text,
+                    tocan1: allAnswers.data[0].correctness,
+                    tocan2: allAnswers.data[1].correctness,
+                    tocan3: allAnswers.data[2].correctness,
+                    toacn4: allAnswers.data[3].correctness
+
                 }));
             }
 
 
         }
+
+
 
 
         this.handleChange = (event) => {
@@ -94,18 +105,53 @@ class GameComponent extends Component {
 
         }
 
+        this.handleAnswer = async (e) => {
+            e.preventDefault();
+            if(this.state.chosenOne != undefined) {
+                console.log("izabran je odgovor idemo provjeirtit")
+                console.log(this.state.chosenOne)
+                if(this.state.chosenOne == 1) {
+                    if(this.state.tocan1 == "DA") {
+                        e.target.className = 'btn btn-success col '
+                    }else {
+                        e.target.className = 'btn btn-error col '
+                    }
+                }if(this.state.chosenOne == 12) {
+                    if(this.state.tocan1 == "DA") {
+                        e.target.className = 'btn btn-success col '
+                    }else {
+                        e.target.className = 'btn btn-error col '
+                    }
+                }if(this.state.chosenOne == 3) {
+                    if(this.state.tocan1 == "DA") {
+                        e.target.className = 'btn btn-success col '
+                    }else {
+                        e.target.className = 'btn btn-error col '
+                    }
+                }if(this.state.chosenOne == 4) {
+                    if(this.state.tocan1 == "DA") {
+                        e.target.className = 'btn btn-success col '
+                    }else {
+                        e.target.className = 'btn btn-error col '
+                    }
+                }
+            } else {
+                console.log("nije izabran niti jedan odgovor") }
+        }
+
         //ovo radi samo u jednom smjeru popravit cu kasnije --Mislav
         //sad radi za sve gumbe i kad se stisne jednom ode u zuto, drugi put kad se stisne u plavo itd...
         //sad bi tu mozda bilo fora staviti da zapamti koji je stisnut i kad se stisne submit se poboja
         // koji je bio tocan a koji netocan na ovu foru samo sa zeleno crveno (success je zeleno, danger je crveno)
         let count1 = 0;
         this.handleColor1 = (e)=>{
+            e.preventDefault();
             console.log("this is working fine");
             this.setState({
-                [e.target.name]: e.target.value
+                chosenOne : e.target.value
             })
             console.log("ovo je chosenOne 1" + this.state.chosenOne);
-            e.preventDefault();
+
             console.log(e.target);
             if (count1 == 0) {
                 e.target.className = 'btn btn-warning col '
@@ -196,12 +242,12 @@ class GameComponent extends Component {
 
                 <div className="button-box text-center container">
                     <div style={{height: 10 + 'rem'}} className={"row"}>
-                            <button value={this.state.chosenOne} id='chosenOne' name={"chosenOne"} type="radio" className='btn btn-info col' onClick={this.handleColor1}>{this.state.odgovor1}</button>
-                            <button value={this.state.chosenOne} id='chosenOne' name={"chosenOne"}  type="radio" className='btn btn-info col' onClick={this.handleColor2}>{this.state.odgovor2}</button>
+                            <button value={1} id='chosenOne' name={"chosenOne"} type="radio" className='btn btn-info col' onClick={this.handleColor1}>{this.state.odgovor1}</button>
+                            <button value={2} id='chosenOne' name={"chosenOne"}  type="radio" className='btn btn-info col' onClick={this.handleColor2}>{this.state.odgovor2}</button>
                     </div>
                     <div style={{height: 10 + 'rem'}} className={"row"}>
-                        <button value={this.state.chosenOne} id='chosenOne' name={"chosenOne"} type="radio" className='btn btn-info col' onClick={this.handleColor3}>{this.state.odgovor3}</button>
-                        <button value={this.state.chosenOne} id='chosenOne' name={"chosenOne"} type="radio" className='btn btn-info col' onClick={this.handleColor4}>{this.state.odgovor4}</button>
+                        <button value={3} id='chosenOne' name={"chosenOne"} type="radio" className='btn btn-info col' onClick={this.handleColor3}>{this.state.odgovor3}</button>
+                        <button value={4} id='chosenOne' name={"chosenOne"} type="radio" className='btn btn-info col' onClick={this.handleColor4}>{this.state.odgovor4}</button>
                     </div>
 
                 </div>
@@ -217,9 +263,13 @@ class GameComponent extends Component {
                 <p>{this.state.kraj} </p>
 
 
+                <form onSubmit={this.handleAnswer}>
+                    <input className="btn btn-secondary" type="submit"/>
+                </form>
                 <form onSubmit={this.handleNext}>
                     <input className="btn btn-primary" type="submit"/>
                 </form>
+
                 <div>
                     {rows && rows}
                 </div>
