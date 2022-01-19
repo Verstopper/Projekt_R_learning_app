a95dc787327f058ec063373a547ebf1faea99956
Mislav Kovacic <mislav.kovacic0201@gmail.com>
2022-01-19 10:46:56 +0100

    navbar fix rendering

--- src/main/frontend/app/src/component/Navbar.js
+++ src/main/frontend/app/src/component/Navbar.js
@@ -18,13 +18,13 @@ class NavBar extends Component {
         }
 
         return (<header>
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <nav className="navbar navbar-expand navbar-light bg-light">
                     <a className="navbar-brand" href="/">Početna stranica</a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse"
-                            data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
-                            aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+                    {/*<button className="navbar-toggler" type="button" data-toggle="collapse"*/}
+                    {/*        data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"*/}
+                    {/*        aria-label="Toggle navigation">*/}
+                    {/*    <span className="navbar-toggler-icon"></span>*/}
+                    {/*</button>*/}
                     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                         <div className="navbar-nav">
                             {!isUserLoggedIn && <>
